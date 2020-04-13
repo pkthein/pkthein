bca4cbd72872df67e108b966f6097fc12eca887d
Htut <phyo.htut@windriver.com>
2020-04-13 09:37:14 -0700

    adding more photo
    Signed-off-by: Htut <phyo.htut@windriver.com>

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,43 @@
 <template>
   <q-page class="">
-    <div class="flex flex-center q-mt-md">
-      <q-avatar size="300px">
+    <div class="q-mt-md">
+      <!-- <q-avatar size="300px">
         <img src="../statics/Me.jpg">
-      </q-avatar>
+      </q-avatar> -->
+      <q-carousel
+        v-model="slide"
+        swipeable animated
+        navigation padding arrows
+        infinite
+        :autoplay="5000"
+        transition-prev="fade"
+        transition-next="fade"
+        control-color="primary"
+      >
+        <q-carousel-slide :name="1" class="column no-wrap flex-center">
+          <div class="" >
+            <q-avatar size="300px">
+              <img src="../statics/CasualMe.jpg">
+            </q-avatar>
+          </div>
+        </q-carousel-slide>
+
+        <q-carousel-slide :name="2" class="column no-wrap flex-center">
+          <div class="">
+            <q-avatar size="300px">
+              <img src="../statics/SeriousMe.jpg">
+            </q-avatar>
+          </div>
+        </q-carousel-slide>
+
+        <q-carousel-slide :name="3" class="column no-wrap flex-center">
+          <div class="" >
+            <q-avatar size="300px">
+              <img src="../statics/Me.jpg">
+            </q-avatar>
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
     </div>
 
     <div class="q-pa-md q-gutter-md">
@@ -28,7 +62,7 @@
       <div align="left" class="q-px-md q-pt-sm">
         Hello all! My name is Phyo Htut, and I am currently attending San Francisco State University (SFSU) for M.S in Computer Science. As for the focus, I am focusing on web development and internet applications. I did my undergrad in Electrical Engineering & Computer Science (EECS) at University of California, Berkeley (UCB).
         <br /><br />
-        In my freetime, I enjoy watching League of Legends (LoL), and playing turn-based strategy games. That being said, I am quite certain I will be re-joining the rift once Wild Rift launches. The primary reason for me quiting LoL was due to the fact that LoL client is a memory hog, and it always find a way to lag out during champion selection. Aside from gaming, I love cats, and soccer.
+        In my freetime, I enjoy watching League of Legends (LoL), and playing turn-based strategy games. That being said, I am quite certain I will be re-joining the rift once Wild Rift launches. Aside from gaming, I love cats, and soccer.
         <br />
       </div>
     </div><br />
@@ -37,5 +71,10 @@
 
 <script>
 export default {
+  data () {
+    return {
+      slide: 1
+    }
+  }
 }
 </script>
