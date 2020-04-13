e16105ff88f34a88b0c0368c1f0c902776558fde
Htut <phyo.htut@windriver.com>
2020-04-13 01:10:05 -0700

    adding resume

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,103 +1,127 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
+  <q-layout view="hHh lpR fFf">
 
+    <q-header reveal elevated class="bg-primary text-white" height-hint="98">
+      <!-- <q-toolbar>
         <q-toolbar-title>
-          Quasar App
+          Welcome
         </q-toolbar-title>
+      </q-toolbar> -->
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
+      <q-tabs align="left">
+        <q-route-tab no-caps to="/me" label="About Me" />
+
+        <q-separator vertical stretch />
+
+        <q-route-tab no-caps to="/experiences" label="Experiences" />
+
+        <q-separator vertical stretch />
+
+        <q-route-tab no-caps to="/resume" label="Resume" />
+
+        <q-separator vertical stretch />
+      </q-tabs>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
+    <q-page-container style="min-height: 87.5vh;">
       <router-view />
     </q-page-container>
+
+    <div class="bg-primary" style="min-height: 90px;" >
+      <div class="row" style="width: 98%; margin: 0 auto;">
+        <div class="q-pa-md">
+          <!-- <div class="text-white" style="font-size: 1.5em;">
+            <div v-if="layoutConfig && layoutConfig.companyName" >
+              Boundless Innovation @ {{ layoutConfig.companyName }}
+            </div>
+
+            <div v-else>
+              Boundless Innovation
+            </div>
+          </div> -->
+
+          <!-- <i
+            class="text-weight-thin text-grey-5"
+            style="font-size: .9em;"
+          >
+            Copyright © 2019 Wind River Systems, Inc.
+          </i> -->
+        </div>
+
+        <q-space />
+
+        <!-- <div class="q-pa-md" align="center">
+          <div
+            class="text-white cursor-pointer"
+            @click="routeTo('/notices')"
+          >
+            Notices
+          </div>
+        </div> -->
+
+        <!-- <div class="q-pa-md" align="center">
+          <div class="text-white cursor-pointer" @click="routeTo('rootAbout')">
+            About Us
+          </div>
+        </div> -->
+
+        <div class="q-pa-md" align="center">
+          <div class="text-white">
+            Follow Me On
+          </div>
+
+          <div>
+            <q-icon
+              class="cursor-pointer" color="white"
+              name="fab fa-github-square" size="2rem"
+              @click="goTo('https://www.github.com/pkthein')"
+            />
+
+            <!-- <q-icon
+              class="cursor-pointer" color="white"
+              name="fab fa-facebook-square" size="2rem"
+              @click="goTo('https://www.facebook.com')"
+            />
+
+            <q-icon
+              class="cursor-pointer" color="white"
+              name="fab fa-instagram" size="2rem"
+              @click="goTo('https://www.instagram.com/')"
+            />
+
+            <q-icon
+              class="cursor-pointer" color="white"
+              name="fab fa-twitter-square" size="2rem"
+              @click="goTo('https://twitter.com/')"
+            /> -->
+
+            <q-icon
+              class="cursor-pointer" color="white"
+              name="fab fa-linkedin" size="2rem"
+              @click="goTo('https://linkedin.com/in/pkthein')"
+            />
+          </div>
+        </div>
+
+      </div>
+    </div>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink'
-
 export default {
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: [
-        {
-          title: 'Docs',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
-        },
-        {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        }
-      ]
+    }
+  },
+  methods: {
+    goTo: function (path) {
+      location.href = path
     }
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+
+</style>
